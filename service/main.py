3598f7da480c975f252458e98607cf920eed7543
GerardoVR7 <GvrZero_07@outlook.com>
2022-11-07 12:33:07 -0600

    fix main to docker

--- service/main.py
+++ service/main.py
@@ -2,15 +2,15 @@ import imp
 from operator import imod
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.activities import activities
-from routes.agencies import agencies
-from routes.tours import tours
-from routes.restaurant import restaurant_router
-from routes.type_dish import type_dish_router
-from routes.menus import menus_router
-from routes.dishes import dishes_router
-from routes.tourist import tourist_router
-from routes.tours_services import tours_services_router
+from .routes.activities import activities
+from .routes.agencies import agencies
+from .routes.tours import tours
+from .routes.restaurant import restaurant_router
+from .routes.type_dish import type_dish_router
+from .routes.menus import menus_router
+from .routes.dishes import dishes_router
+from .routes.tourist import tourist_router
+from .routes.tours_services import tours_services_router
 
 app = FastAPI()
 
