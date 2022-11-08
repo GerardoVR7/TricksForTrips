c2902da5955fabf82336dd4e0b1d384a4a93aecb
GerardoVR7 <GvrZero_07@outlook.com>
2022-11-07 23:39:23 -0600

    create the microservice tours

--- service/main.py
+++ service/main.py
@@ -1,17 +1,11 @@
-import imp
-from operator import imod
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes.mexico_cities import mexico_cities_router
-from .routes.activities import activities
-from .routes.agencies import agencies
-from .routes.tours import tours
-from .routes.restaurant import restaurant_router
-from .routes.type_dish import type_dish_router
-from .routes.menus import menus_router
-from .routes.dishes import dishes_router
-from .routes.tourist import tourist_router
-from .routes.tours_services import tours_services_router
+from routes.mexico_cities import mexico_cities_router
+from routes.activities import activities
+from routes.agencies import agencies
+from routes.tours import tours
+from routes.tours_services import tours_services_router
 
 app = FastAPI()
 
@@ -31,11 +25,6 @@ app.include_router(mexico_cities_router)
 app.include_router(activities)
 app.include_router(agencies)
 app.include_router(tours)
-app.include_router(restaurant_router)
-app.include_router(menus_router)
-app.include_router(type_dish_router)
-app.include_router(dishes_router)
-app.include_router(tourist_router)
 app.include_router(tours_services_router)
 
 @app.get("/")
--- service/schema/menus.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class Menus(BaseModel):
-    id: Optional[int]
-    id_restaurant: int
-    name: str
