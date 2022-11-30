96ec4106174a37a45f294ccf51543a103515b3de
GerardoVR7 <GvrZero_07@outlook.com>
2022-11-30 10:22:14 -0600

    avance

--- service/routes/openpay.py
+++ service/routes/openpay.py
@@ -2,6 +2,7 @@ from unittest import result
 from typing import Dict
 from fastapi import APIRouter, HTTPException, Response, Depends
 from ..config.database import conn
+from ..schema.customer import Customer
 from starlette.status import HTTP_204_NO_CONTENT
 from cryptography.fernet import Fernet
 from datetime import date
@@ -16,11 +17,11 @@ openpay.merchant_id = os.environ["OPENPAY_MERCHANT"]
 openpay_router =  APIRouter()
 url = 'https://sandbox-api.openpay.mx/v1/m3zwajjoabqmrgg9dk3p/'
 @openpay_router.post(url, tags=["Payments"])
-async def create_booking(token):
+async def create_booking(token : Customer):
 
     try:
         print(token)
-        customer = openpay.Customer.create()
+        customer = openpay.Customer.create(token)
         print(customer)
     
     except:
